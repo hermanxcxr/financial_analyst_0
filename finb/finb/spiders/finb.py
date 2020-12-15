a6b2cac3c731d3a62608c2ac32f5becae58297cd
hermanxcxr <hcastillo191085@gmail.com>
2020-12-14 23:00:34 -0500

    preconfiguracion para scraping por cada EXCH_STOCKS

--- finb/finb/spiders/finb.py
+++ finb/finb/spiders/finb.py
@@ -6,15 +6,17 @@ import random
 
 LIST_OF_SEUDONIMS = ['AAA', 'PEPITO', 'MANITO', 'TRADER', 'LOVE', 'MICHI', 'WHAT_ELSE'] 
 
+
 class FinbSpider(scrapy.Spider):
     name = 'finb'
-    start_urls = [
-        'https://eoddata.com'        
-    ]
     custom_settings = {
         'USER_AGENT' : random.choice(LIST_OF_SEUDONIMS)
     }
     
+    start_urls = [
+        'https://eoddata.com'        
+    ]
+
     def parse(self, response):
 
         target = int(self.target)
@@ -34,8 +36,8 @@ class FinbSpider(scrapy.Spider):
             exchange_name = response.xpath('//div[contains(@id,"Exchange")]/table[@class="quotes"]/tr/td[2]').getall()
 
             #borra el contenido del archivo final 
-            with open('../../output/stocks_prev.csv', 'wt', encoding= 'utf-8') as f:
-                f.write("Ticker_Stock,Name_Stock,Ticker_Exch")
+            # with open('../../output/stocks_prev.csv', 'wt', encoding= 'utf-8') as f:
+            #     f.write("Ticker_Stock,Name_Stock,Ticker_Exch")
 
             #print('finb******')
             #print(variable)
@@ -66,22 +68,38 @@ class FinbSpider(scrapy.Spider):
                 #     #print(exchange_lines)
 
         if target == 1:
-
-            with open('output/exchange.csv', 'r', encoding= 'utf-8') as f:
-                exchange_lines = f.readlines()
-                n = len(exchange_lines) # este numero debe ir a pipe_00 de algun modo
-                print(exchange_lines[2])
             
-            exchange_url = response.xpath('//div[contains(@id,"Exchange")]/table[@class="quotes"]/tr/td/a[contains(@title,"Browse")]/@href').getall()
-            exchange_ticker = response.xpath('//div[contains(@id,"Exchange")]/table[@class="quotes"]/tr/td/a/text()').getall()
+            with open('../../output/exchange.csv', 'r', encoding= 'utf-8') as f:
+                exchanges = f.readlines()
+            
+            exchange = exchanges[target]
+            exchange_params = list(exchange.split(","))
+            exchange_params[2] = exchange_params[2].replace("\n","")
+            print(exchange_params)
+            print(exchange)
+            
+            str_filename =  f'../../output/{exchange_params[0]}_stocks.csv'
+            #borra el contenido del archivo final 
+            with open(str_filename, 'wt', encoding= 'utf-8') as f:
+                f.write("Ticker_Stock,Name_Stock,Ticker_Exch")
 
-            for url,exc_ticker in zip(exchange_url,exchange_ticker):
-                url_complete = response.urljoin(url)
-                yield response.follow(
-                    url_complete,
-                    callback = self.parse_link,
-                    cb_kwargs = { "url" : url_complete, "exc_ticker" : exc_ticker}
-                )
+            yield response.follow(
+                exchange_params[2],
+                callback = self.parse_link,
+                cb_kwargs = {"url": exchange_params[2], "exc_ticker" : exchange_params[0], "exc_name" : exchange_params[1] }
+            )
+
+
+            # exchange_url = response.xpath('//div[contains(@id,"Exchange")]/table[@class="quotes"]/tr/td/a[contains(@title,"Browse")]/@href').getall()
+            # exchange_ticker = response.xpath('//div[contains(@id,"Exchange")]/table[@class="quotes"]/tr/td/a/text()').getall()
+
+            # for url,exc_ticker in zip(exchange_url,exchange_ticker):
+            #     url_complete = response.urljoin(url)
+            #     yield response.follow(
+            #         url_complete,
+            #         callback = self.parse_link,
+            #         cb_kwargs = { "url" : url_complete, "exc_ticker" : exc_ticker}
+            #     )
 
     def parse_link(self,response, **kwargs):
         '''parse_link se ubica en la primera página de cada Exchange mkt
@@ -91,6 +109,7 @@ class FinbSpider(scrapy.Spider):
         '''    
         #exchange_page = kwargs["url"]
         exc_ticker = kwargs["exc_ticker"]
+        exc_name = kwargs["exc_name"]
 
         exchange_page_urls = response.xpath('//td[@class="ld"]/a/@href').getall()
         
@@ -99,7 +118,7 @@ class FinbSpider(scrapy.Spider):
             yield response.follow(
                 url_complete,
                 callback = self.parse_stocks,
-                cb_kwargs = { "url" : url_complete, "exc_ticker" : exc_ticker}
+                cb_kwargs = { "url" : url_complete, "exc_ticker" : exc_ticker, "exc_name" : exc_name}
             )
 
     def parse_stocks(self,response,**kwargs):
@@ -112,7 +131,11 @@ class FinbSpider(scrapy.Spider):
         stocks_tickers = response.xpath('//tr[contains(@class,"ro") or contains(@class,"re")]/td/a[contains(@title,"Display Quote")]/text()').getall()
         stocks_names = response.xpath('//tr[contains(@class,"ro") or contains(@class,"re")]/td[not(@align)]/text()').getall()
         
-        with open('../../output/stocks_prev.csv', 'at', encoding= 'utf-8') as f:
+        exc_ticker = kwargs["exc_ticker"]
+        str_filename =  f'../../output/{exc_ticker}_stocks.csv'
+        print(str_filename)
+        #borra el contenido del archivo final 
+        with open(str_filename, 'at', encoding= 'utf-8') as f:
             f.write("\n")
             for ticker, name in zip(stocks_tickers,stocks_names):
                 f.write(ticker)
--- pipe_00.py
+++ pipe_00.py
@@ -2,18 +2,30 @@
 import subprocess
 import sys
 
+with open('temp_output/exchanges_length.csv','rt',encoding='utf-8') as f:
+    EXCHANGES_QUANTITY = int(f.read())
+
 def run(*args):
     print(args)
     print(type(args[0][1]))
+    #print(EXCHANGES_QUANTITY)
     activador_proxy = int(args[0][1])
     if activador_proxy == 0:
         #procedimiento de obtencion de EXCHANGES
         subprocess.run(['scrapy','crawl','proxyfinder'],cwd='./proxy_finder/proxy_finder')
         #subprocess.run(['mv','proxies_list.txt','temp_output/']) YA NO ES NECESARIO SE UBICA EL ARCHIVO DIRECTAMENTE
         subprocess.run(['python','depurador_proxies.py'],cwd='./operaciones')
-        subprocess.run(['scrapy','crawl','finb','-a','target=0'],cwd='./finb/finb')
+        #subprocess.run(['scrapy','crawl','finb','-a','target=0'],cwd='./finb/finb')
         #probable ciclo for en base al resultado esperado de la cantidad de EXCHANGES descargados repitiendo el proceso anterior
+        subprocess.run(['scrapy','crawl','proxyfinder'],cwd='./proxy_finder/proxy_finder')
+        subprocess.run(['python','depurador_proxies.py'],cwd='./operaciones')
+        subprocess.run(['scrapy','crawl','finb','-a','target=1'],cwd='./finb/finb')
     elif activador_proxy == 1:
+        subprocess.run(['scrapy','crawl','proxyfinder'],cwd='./proxy_finder/proxy_finder')
+        subprocess.run(['python','depurador_proxies.py'],cwd='./operaciones')
+        subprocess.run(['scrapy','crawl','finb','-a','target=1'],cwd='./finb/finb')
+        for i in range(1,EXCHANGES_QUANTITY+1):
+            print(i)
         print('ensayo')
     else:
         print("Ni idea porqué no funcionó")
